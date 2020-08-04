7be5a4138aa63e0b821f756cf4b7fd9f19ab164c
Pedro H. Q. Pirralha <46597812+PedroPirralha@users.noreply.github.com>
2020-08-04 20:05:03 -0300

    Add files via upload
    tabela de cores ansi

---
+++ tabelaDeCores2.0.py
@@ -0,0 +1,25 @@
+vermelho = '\033[31m'
+verde = '\033[32m'
+azul = '\033[34m'
+
+ciano = '\033[36m'
+magenta = '\033[35m'
+amarelo = '\033[33m'
+preto = '\033[30m'
+
+branco = '\033[37m'
+
+clear = '\033[0;0m'
+negrito = '\033[1m'
+reverso = '\033[2m'
+
+fundoPreto = '\033[40m'
+fundoVermelho = '\033[41m'
+fundoVerde = '\033[42m'
+fundoAmarelo = '\033[43m'
+fundoAzul = '\033[44m'
+fundoMagenta = '\033[45m'
+fundoCiano = '\033[46m'
+fundoBranco = '\033[47m'
+
+print(f'{fundoMagenta}fundo magenta{clear}{verde}Letra verde{clear}')
